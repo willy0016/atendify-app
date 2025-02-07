e7fd3ea460ad5ccbab23f06258193e1c3d6b4354
Mateus Borges <mateusguimaraes717@gmail.com>
2025-02-07 11:19:36 -0300

    configuração incial do app atendify

---
+++ src/screens/Home/index.tsx
@@ -0,0 +1,12 @@
+import { Text, View } from 'react-native';
+import {styles} from "./styles";
+
+export function Home() {
+    return (
+        <View style={styles.container}>
+            <Text>Atendify 2025</Text>
+        </View>
+    );
+}
+
+
---
+++ src/styles/theme.ts
@@ -0,0 +1,20 @@
+export default {
+    colors: {
+        primary300: '#140623',
+        primary200: '#4C1F7A',
+        primary100: '#D0BBE5',
+
+        highlight1: '#E33629',
+        highlight2: '#FF8000',
+        highlight3: '#219B9D',
+
+        gray200: '#867F8D',
+        gray50: '#EEEEEE',
+
+        black: '#000000',
+        white: '#ffffff',
+    },
+    fonts: {
+
+    }
+}
