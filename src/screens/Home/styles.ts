f92e82dc917de393689dd66839ff7dea23a7e899
Willy Brener <94143283+willy0016@users.noreply.github.com>
2025-02-10 16:12:50 -0300

    Update styles.ts

--- src/screens/Home/styles.ts
+++ src/screens/Home/styles.ts
@@ -3,7 +3,7 @@ import styled from "styled-components/native"
 
 export const Container = styled.View`
     flex: 1;
-    background-color: ${({ theme }) => theme.colors.gray50};
+    background-color: ${ theme.colors.gray50};
     align-items: center;
     justify-content: center;
 `;
